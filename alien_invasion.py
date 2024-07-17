9f4511f756a289d70c96084714ad8d959c7f9aca
Jessica Arruda <sicarruda@gmail.com>
2024-07-17 18:19:50 -0300

    Cria settings para configurar opções de definição de tela

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior.
@@ -8,20 +10,36 @@ class AlienInvasion:
     def __init__(self):
         # Initialize the game, and create game resources.
         pygame.init()
+
         self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+
         pygame.display.set_caption("Attack Invasion")
+        
+        # Set the background color.
+        # self.bg_color = (150, 150, 150)
 
     def run_game(self):
         # Start the main loop for the game.
         while True:
             # Watch for keyboard and mouse events.
-            
+
             for event in pygame.event.get():
-                
+
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
