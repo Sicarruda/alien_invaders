b63810b6bcd201bfe38950976537b1b18afde1e9
Jessica Arruda <sicarruda@gmail.com>
2024-07-17 18:47:20 -0300

    Adiciona imagem da nave a tela do jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -20,6 +21,8 @@ class AlienInvasion:
         )
 
         pygame.display.set_caption("Attack Invasion")
+
+        self.ship = Ship(self)
         
         # Set the background color.
         # self.bg_color = (150, 150, 150)
@@ -36,6 +39,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
