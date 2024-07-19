9b3761d63f34b447851f0ab72c16122949b6231a
Jessica Arruda <sicarruda@gmail.com>
2024-07-19 16:37:29 -0300

    Adiciona config para projeteis

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Settings:
     # A class to store all settings for Alien Invasion.
-    
+
     def __init__(self):
         # Initialize the game's settings.
 
@@ -8,7 +8,15 @@ class Settings:
         self.screen_width = 0
         self.screen_height = 0
         self.screen_width_standard = 1200
-        self.screen_height_standard= 800
-        self.bg_color = (230,230,230)
-        self.ship_speed = 7
+        self.screen_height_standard = 800
+        self.bg_color = (230, 230, 230)
         self.fullscreen_mode = False
+
+        # Ship settings:
+        self.ship_speed = 7
+
+        # Bullet settings:
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
